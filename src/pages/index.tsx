14c978994bf63c24e3ad5efe15ae3fb53e992046
seung-heee <tmdgmldi2595@naver.com>
2025-01-10 15:47:57 +0900

    chore: TailwindCSS 초기 설정 및 기본 세팅 완료

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,33 +1,23 @@
+import Image from 'next/image';
 const METRIC = {
-    BG_HEIGHT: 5000,
-    APPLE_HEIGHT: 50,
-}
+  BG_HEIGHT: 5000,
+  APPLE_HEIGHT: 50,
+};
 
 export default function Home() {
-    return (
-        <>
-            <div
-                style={{position: 'relative', width: '100%', height: METRIC.BG_HEIGHT}}
-            >
-                <img
-                    src={'/background.png'}
-                    width={'100%'}
-                    height={METRIC.BG_HEIGHT}
-                    draggable={false}
-                />
-                <div
-                    style={{
-                        position: 'absolute',
-                        left: 0,
-                        right: 0,
-                        height: 30,
-                        backgroundColor: '#EC083F',
-                        // FIXME: top 값을 적절히 조절하여 도착선과 사과의 시작점 사이의 거리가 4000px 차이나게 해주세요
-                        // 현재는 도착선을 임의로 최하단에 붙여놨습니다
-                        top: 5000,
-                    }}
-                />
-            </div>
-        </>
-    );
+  return (
+    <>
+      <div className="relative w-full" style={{ height: METRIC.BG_HEIGHT }}>
+        <Image src="/background.png" alt="Background" draggable={false} width={1920} height={METRIC.BG_HEIGHT} className="w-full h-auto" />
+        <div
+          className="absolute left-0 right-0 h-[30px] bg-[#EC083F]"
+          style={{
+            // FIXME: top 값을 적절히 조절하여 도착선과 사과의 시작점 사이의 거리가 4000px 차이나게 해주세요
+            // 현재는 도착선을 임의로 최하단에 붙여놨습니다
+            top: 5000,
+          }}
+        />
+      </div>
+    </>
+  );
 }
