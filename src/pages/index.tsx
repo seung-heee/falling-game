bcaab45af7fed6e6b6e67c80dd5e87b6147181e6
seung-heee <tmdgmldi2595@naver.com>
2025-01-14 16:01:46 +0900

    Feat: 배경&도착선 animated
    사과 고정된 채 배경&도착선 위 쪽으로 이동하는 애니메이션 구현

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,7 @@
 import AppleComponent from '@/components/AppleComponent';
 import { useEffect, useRef } from 'react';
 import Image from 'next/image';
+import { useSpring, animated } from '@react-spring/web';
 
 const METRIC = {
   BG_HEIGHT: 6000,
@@ -10,33 +11,41 @@ const METRIC = {
 export default function Home() {
   const backgroundRef = useRef<HTMLImageElement>(null);
 
+  // 애니메이션 상태
+  const [{ top }, api] = useSpring(() => ({
+    top: 0, // 초기 위치
+    config: { duration: 5000 }, // 애니메이션 속도: 5000ms (5초)
+  }));
+
   useEffect(() => {
-    console.log(backgroundRef.current?.naturalHeight);
-    console.log(backgroundRef.current?.clientHeight);
-  }, []);
+    // 애니메이션 시작: 4230px 위로 이동
+    api.start({ top: -4230 });
+  }, [api]);
 
   return (
-    <>
-      <div style={{ position: 'relative', width: '100%', height: METRIC.BG_HEIGHT }}>
-        {/* 배경 */}
-        <Image src="/background.png" alt="Background" width={480} height={METRIC.BG_HEIGHT} draggable={false} />
-        {/* <img ref={backgroundRef} src={'/background.png'} width={'100%'} height={METRIC.BG_HEIGHT} draggable={false} /> */}
+    <div className="relative w-full" style={{ height: METRIC.BG_HEIGHT }}>
+      {/* 배경 */}
+      <animated.div
+        className="absolute w-full"
+        style={{
+          height: METRIC.BG_HEIGHT,
+          transform: top.to((t) => `translateY(${t}px)`),
+        }}
+      >
+        <Image ref={backgroundRef} src="/background.png" alt="Background" width={480} height={METRIC.BG_HEIGHT} draggable={false} />
+      </animated.div>
 
-        {/* 도착선 */}
-        <div
-          style={{
-            position: 'absolute',
-            left: 0,
-            right: 0,
-            height: 30,
-            backgroundColor: '#EC083F',
-            top: 4350,
-          }}
-        />
+      {/* 도착선 */}
+      <animated.div
+        className="absolute left-0 right-0 h-[30px] bg-[#EC083F]"
+        style={{
+          top: 4350, // 도착선의 초기 위치
+          transform: top.to((t) => `translateY(${t}px)`),
+        }}
+      />
 
-        {/* 사과 */}
-        <AppleComponent top={300} />
-      </div>
-    </>
+      {/* 사과 */}
+      <AppleComponent top={300} />
+    </div>
   );
 }
