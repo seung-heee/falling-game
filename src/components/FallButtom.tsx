50f2cac1cc0064ac4a760196cfb495991eff1379
seung-heee <tmdgmldi2595@naver.com>
2025-01-14 17:35:12 +0900

    Fix: 이미지 꾹 눌렀을 때 기본동작차단

--- src/components/FallButtom.tsx
+++ src/components/FallButtom.tsx
@@ -12,13 +12,14 @@ interface FallButtonProps {
 export const FallButton: React.FC<FallButtonProps> = ({ top, onStart, onStop, style }) => (
   <animated.button
     draggable={false}
-    className="absolute right-12 no-drag"
+    className="absolute right-12 no-tap-highlight"
     style={{ top, ...style }}
     onMouseDown={onStart}
     onMouseUp={onStop}
     onTouchStart={onStart} // 모바일 대응
     onTouchEnd={onStop}
     onDragStart={(e) => e.preventDefault()}
+    onContextMenu={(e) => e.preventDefault()}
   >
     <Image src="/click-please.png" alt="꾹 눌러주세요~" width={110} height={100} />
   </animated.button>
